d546fa979113f11b9600524c243583acd3abe438
AndreeaCiomag <92028999+AndreeaCiomag@users.noreply.github.com>
2022-10-11 18:41:34 +0300

    v1

---
+++ Ciomag_Andreea_Lab1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Ciomag_Andreea_Lab1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
